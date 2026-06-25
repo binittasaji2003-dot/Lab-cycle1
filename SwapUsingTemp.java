d4b7b1e1e1a01b116efbbff83e5ba35e3c6b1f74
binittasaji2003-dot <binittasaji2003@gmail.com>
2026-06-25 13:03:12 +0530

    first commit

---
+++ SwapUsingTemp.java
@@ -0,0 +1,19 @@
+public class SwapUsingTemp {
+    public static void main(String[] args) {
+
+        int A = 15;
+        int B = 25;
+
+        System.out.println("Before Swap");
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+
+        int temp = A;
+        A = B;
+        B = temp;
+
+        System.out.println("\nAfter Swap");
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+    }
+}
