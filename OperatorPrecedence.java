c709eb4b52c850e531d0ff13f6e82d1993fa2eb7
binittasaji2003-dot <binittasaji2003@gmail.com>
2026-07-02 00:35:25 +0530

    Java Lab cycle programs

--- OperatorPrecedence.java
+++ OperatorPrecedence.java
@@ -1,14 +1,31 @@
+//Lab Cycle 1
+// Task 10: Operator Precedence Evaluation
+// 1/07/2026
+import java.util.Scanner;
+
 public class OperatorPrecedence {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int a = 10;
-        int b = 5;
-        int c = 2;
-        int d = 8;
-        int e = 4;
+        System.out.print("a = ");
+        int a = sc.nextInt();
+
+        System.out.print("b = ");
+        int b = sc.nextInt();
+
+        System.out.print("c = ");
+        int c = sc.nextInt();
+
+        System.out.print("d = ");
+        int d = sc.nextInt();
+
+        System.out.print("e = ");
+        int e = sc.nextInt();
 
         int result = a + b * c - d / e;
 
         System.out.println("Result = " + result);
+
+        sc.close();
     }
 }
--- SwapUsingTemp.java
+++ SwapUsingTemp.java
@@ -1,19 +1,30 @@
+//Lab Cycle 1
+// Task 9: Swap Two Numbers
+// 1/07/2026
+import java.util.Scanner;
+
 public class SwapUsingTemp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int A = 15;
-        int B = 25;
+        System.out.print("A = ");
+        int a = sc.nextInt();
+
+        System.out.print("B = ");
+        int b = sc.nextInt();
 
         System.out.println("Before Swap");
-        System.out.println("A = " + A);
-        System.out.println("B = " + B);
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
 
-        int temp = A;
-        A = B;
-        B = temp;
+        int temp = a;
+        a = b;
+        b = temp;
 
         System.out.println("\nAfter Swap");
-        System.out.println("A = " + A);
-        System.out.println("B = " + B);
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+
+        sc.close();
     }
 }
